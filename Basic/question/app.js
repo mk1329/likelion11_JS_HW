bca902e81aadf76e3a5543a8020303093fc4fccb
mk1329 <mk1329@dgu.ac.kr>
2023-05-16 22:19:32 +0900

    6주차과제2번_JS Console

--- Basic/question/app.js
+++ Basic/question/app.js
@@ -8,19 +8,19 @@ const arr2 = [
 ]
 
 // Q1-1. arr에서 3출력 (Hint:인덱싱은 0부터 시작!)
-
+console.log(arr[2])
 
 
 // Q1-2. arr에서 [1,2,3]출력 (Hint:Array.js에서 slice함수를 사용해볼까요?)
-
+console.log(arr.slice(0,3))
 
 
 // Q1-3. arr2에서 [8, 9]출력 (Hint:배열 안에 원소가 배열일 뿐!)
-
+console.log(arr2[2])
 
 
 // Q1-4. arr2에서 [4, 5, 6]출력 (Hint:원소안 원소!)
-
+console.log(arr2[1][2])
 
 
 
@@ -68,19 +68,19 @@ const obj = {
 }
 
 // Q2-1. 서희찬의 나이를 obj객체로부터 출력하기 (Hint:obj.~~)
-
+console.log(obj.age)
 
 
 // Q2-2. 서희찬의 Computer 학점(A+)을 obj객체로부터 출력하기
-
+console.log(obj.gpa.Computer)
 
 
 // Q2-3. 서희찬의 친구인 밥준서의 나이(21)를 obj객체로부터 출력하기 (Hint:obj안에 배열)
-
+console.log(obj.friends[0].age)
 
 
 // Q2-4. 서희찬의 친구인 슬기요미가 수강한 강의 중에서 "인강공학"을 obj객체로부터 출력하기
-
+console.log(obj.friends[1].lecture[1][2])
 
 
 
@@ -97,12 +97,18 @@ const arr3 = [1,2,3,4,5,6];
 
 
 // Q3-1. map함수와 arr3을 활용하여 [10,20,30,40,50,60]을 출력하시오.
-
+var arr_map = arr3.map(function(x){
+    return x*10
+})
+console.log(arr_map)
+// -> console.log(arr3.map( x => x*10 ))
 
 
 // Q3-2. filter함수와 arr3을 활용하여 [1,3,5]출력하기 (Hint:홀수와 짝수 구분은 "%2"를 활용)
-
-
+var arr_filter = arr3.filter(function(x){
+    return x%2==1
+})
+console.log(arr_filter)
 
 
 
@@ -127,10 +133,18 @@ console.log(isLeap(2020));  -> 윤년이 맞습니다.
 console.log(isLeap(2021));  -> 윤년이 아닙니다.
 */
 
+function isLeap(a) {
+    if (a%4==0 && a%100!==0 || a%400==0) {
+        return "윤년이 맞습니다.";
+    }
+    else {
+        return "윤년이 아닙니다.";
+    }
+}
 
+console.log(isLeap(2020));  //<- 다 풀고 주석 풀어주세요!
 
-// console.log(isLeap(2020));  <- 다 풀고 주석 풀어주세요!
-// console.log(isLeap(2021));  <- 다 풀고 주석 풀어주세요!
+console.log(isLeap(2021));  //<- 다 풀고 주석 풀어주세요!
 
 
 console.log("======= 문제 구분선 =======");
@@ -151,7 +165,9 @@ console.log("Q5 반복문 연습문제1");
 (Hint:for문 or while문 사용 + console.log("*")사용)
 */
 
-
+for (var i=0; i<5; i++) {
+    console.log("*".repeat(i+1));
+}
 
 
 
@@ -172,7 +188,9 @@ console.log("Q6 반복문 연습문제2");
 (Hint:console.log(" ")로 빈칸 표현)
 */
 
-
+for (var i=5; i>0; i--) {
+    console.log(" ".repeat(i) + "*".repeat(6-i));
+}
 
 
 console.log("======= 문제 구분선 =======");
@@ -192,8 +210,22 @@ console.log("Q7 반복문 연습문제3");
 (Hint:이중for문 활용)
 */
 
+for (var i=0; i<5; i++) {
+    var star = "*";
+    var space = "";
+    for (var j=0; j<4-i; j++) {
+        space += " ";
+    }
+    for (var k=0; k<i; k++) {
+        star += "*"+"*";
+    }
+    console.log(space+star+"\n");
+}
 
-
+// Q7 -> repeat 함수 사용해서 표현
+/*for (var i=0; i<5; i++) {
+    console.log(" ".repeat(4-i)+"*".repeat(2*i+1))
+}*/
 
 console.log("======= 문제 구분선 =======");
 
@@ -218,5 +250,33 @@ console.log("Q8 반복문 연습문제4");
 */
 
 
+for (var i=0; i<5; i++) {
+    var star = "*";
+    var space = "";
+    for (var j=0; j<4-i; j++) {
+        space += " ";
+    }
+    for (var k=0; k<i; k++) {
+        star += "*"+"*";
+    }
+    console.log(space+star+"\n");
+}
+for (var i=4; i>0; i--) {
+    var star = "*";
+    var space = " ";
+    for (var j=0; j<4-i; j++) {
+        space += " ";
+    }
+    for (var k=0; k<i-1; k++) {
+        star += "*"+"*";
+    }
+    console.log(space+star+"\n");
+}
 
-
+// Q8 -> repeat 함수 사용해서 표현
+/*for (var i=0; i<5; i++) {
+    console.log(" ".repeat(4-i)+"*".repeat(2*i+1))
+}
+for (var i=4; i>0; i--) {
+    console.log(" ".repeat(5-i)+"*".repeat(2*i-1))
+}*/
